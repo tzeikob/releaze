be9be88330ef5fabb371bb46a173598ce9b4bea4
Jake Ob <iakopap@gmail.com>
2021-06-01 20:05:56 +0300

    Add feature to print commit messages in dry run (#1)
    Use the git process to print commit messages of the git repository
    to the std output in a dry run. The format should be like so:
    
    ```
    <hash> <head-message>
    <hash> <head-message>
    ...
    ```
    
    In addition the command should support to options, `--from` and
    `--to`, those is expected to be the hash range within which each
    commit should fall in. In case neither `--from` neither `--to` are
    given the cli should print all the commits.
    
    Resolves #1

---
+++ bin/verzion.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+'use strict';
+
+const { version } = require('../package.json');
+
+function onFatalError(error) {
+  process.exitCode = 2;
+
+  console.error(`Oops,something went wrong! \nVerzion: ${version}\n${error}`);
+  console.error(error);
+}
+
+(async function main() {
+  process.on('uncaughtException', onFatalError);
+  process.on('unhandledRejection', onFatalError);
+
+  process.exitCode = await require('../lib/cli.js');
+}()).catch(onFatalError);
