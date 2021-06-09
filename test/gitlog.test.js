e166b109200340cb079fa9391c0fab52afe379af
Jake Ob <iakopap@gmail.com>
2021-06-09 16:45:28 +0300

    Add tests for gitlog to assert error handling for execFile (refs #5)
    Asserts that error handling is done correctly for any possible exceptions
    of stderr or fatal Error the `execFile` function may throw in order to execute
    the `git log` command.
    
    Refs #5

--- test/gitlog.test.js
+++ test/gitlog.test.js
@@ -1,4 +1,16 @@
-const gitlog = require('../lib/gitlog');
+jest.mock('util');
+jest.mock('child_process');
+
+const { promisify } = require('util');
+const { execFile } = require('child_process');
+
+promisify.mockImplementation((fn) => fn);
+
+const gitlog = require('../lib/gitlog.js');
+
+afterEach(() => {
+  execFile.mockReset();
+});
 
 describe('Error handling in gitlog({ from, to , format })', () => {
   test('should reject with Error when `from` arg is anything but a non empty string', async () => {
@@ -44,5 +56,35 @@ describe('Error handling in gitlog({ from, to , format })', () => {
     await expect(gitlog({ format: (() => {}) })).rejects.toBeInstanceOf(Error);
     await expect(gitlog({ format: Symbol('sym') })).rejects.toBeInstanceOf(Error);
     await expect(gitlog({ format: '' })).rejects.toBeInstanceOf(Error);
+  });
+
+  test('should reject with Error when `git log` process returns stderr as Error', async () => {
+    execFile.mockReturnValue(Promise.resolve({ stderr: new Error('A stderr occurred') }));
+
+    expect.assertions(3);
+
+    await expect(gitlog({})).rejects.toBeInstanceOf(Error);
+    await expect(gitlog({})).rejects.toHaveProperty('message', 'Error: A stderr occurred');
+    expect(execFile).toHaveBeenCalledTimes(2);
+  });
+
+  test('should reject with Error when `git log` process returns stderr as non Error', async () => {
+    execFile.mockReturnValue(Promise.resolve({ stderr: 'A stderr occurred' }));
+
+    expect.assertions(3);
+
+    await expect(gitlog({})).rejects.toBeInstanceOf(Error);
+    await expect(gitlog({})).rejects.toHaveProperty('message', 'A stderr occurred');
+    expect(execFile).toHaveBeenCalledTimes(2);
+  });
+
+  test('should reject with Error when `git log` process throws a fatal error', async () => {
+    execFile.mockReturnValue(Promise.reject(new Error('A fatal error occurred')));
+
+    expect.assertions(3);
+
+    await expect(gitlog({})).rejects.toBeInstanceOf(Error);
+    await expect(gitlog({})).rejects.toHaveProperty('message', 'A fatal error occurred');
+    expect(execFile).toHaveBeenCalledTimes(2);
   });
 });
