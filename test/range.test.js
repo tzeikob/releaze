4d91ca082cb8fe5699d624b5c8578d14fe4e3a51
Jake Ob <iakopap@gmail.com>
2021-06-23 14:48:37 +0300

    Add more tests to the range operation (refs #12)
    Completes testing for the `range` operation adding more tests,
    
     * get last stable for stable releases,
     * get the last either stable or prerelease for prerelease releases,
     * test for semver unordered tag list,
     * test for not semver valid given tag names
    
    Refs #12

--- test/range.test.js
+++ test/range.test.js
@@ -96,8 +96,8 @@ describe('Range should spawn once a `git tag` process', () => {
   });
 });
 
-describe('Range called for stable should resolve to', () => {
-  test('an empty object in case no tags found in the repository', async () => {
+describe('Range called for a stable release should resolve to', () => {
+  test('an empty range object in case no tags found in the repository', async () => {
     expect.assertions(1);
 
     execFile.mockReturnValue(Promise.resolve({ stdout: '' }));
@@ -105,23 +105,97 @@ describe('Range called for stable should resolve to', () => {
     await expect(range()).resolves.toEqual({});
   });
 
-  test('an object with `from` to last stable tag if last tag is a prerelease', async () => {
-    expect.assertions(1);
+  test('a range with `from` to the last stable tag ignoring any intermediate prereleased tags', async () => {
+    expect.assertions(4);
 
-    const tags = ['v0.1.0', 'v0.1.1-alpha.0', 'v0.1.1-alpha.1', 'v1.0.0', 'v1.0.1-aplha.0', 'v1.0.1-aplha.1'];
-
+    let tags = ['v1.0.0', 'v1.1.0-rc.0', 'v1.1.0-rc.1', 'v1.1.0'];
     execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
 
-    await expect(range()).resolves.toEqual({ from: 'v1.0.0', to: 'HEAD' });
+    await expect(range()).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+
+    tags = ['v1.0.0', 'v1.1.0-rc.0', 'v1.1.0-rc.1', 'v1.1.0', 'v1.2.0-rc.0'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range()).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+
+    tags = ['v1.1.0', 'v1.2.0-rc.0', 'v1.2.0-rc.1', 'v1.2.0-rc.2'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range()).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+
+    tags = ['v1.0.0', 'v1.1.0-rc.0', 'v1.1.0', 'v1.2.0-rc.0', 'v1.2.0-rc.1'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range()).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+  });
+
+  test('a valid range object even though tags are given in a not sorted semver order', async () => {
+    expect.assertions(1);
+
+    let tags = ['v1.1.0', 'v1.1.0-rc.0', 'v1.0.0', 'v1.1.0-rc.1'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range()).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+  });
+
+  test('a valid range object filtering out any tags given a not semver compatible name', async () => {
+    expect.assertions(1);
+
+    let tags = ['v1.1.0', 'exp-0', 'v1.1.0-rc.0', 'v1.0.0', 'v1.1.0-rc.1', 'stable'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range()).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
   });
 });
 
-describe('Range called for prerelease should resolve to', () => {
-  test('an empty object in case no tags found in the repository', async () => {
+describe('Range called for a prerelease release should resolve to', () => {
+  test('an empty range object in case no tags found in the repository', async () => {
     expect.assertions(1);
 
     execFile.mockReturnValue(Promise.resolve({ stdout: '' }));
 
     await expect(range(true)).resolves.toEqual({});
+  });
+
+  test('a range with `from` to the last tag either a stable or a prereleased tag', async () => {
+    expect.assertions(4);
+
+    let tags = ['v1.0.0', 'v1.1.0-rc.0', 'v1.1.0-rc.1', 'v1.1.0'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range(true)).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+
+    tags = ['v1.0.0', 'v1.1.0-rc.0', 'v1.1.0-rc.1', 'v1.1.0', 'v1.2.0-rc.0'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range(true)).resolves.toEqual({ from: 'v1.2.0-rc.0', to: 'HEAD' });
+
+    tags = ['v1.1.0', 'v1.2.0-rc.0', 'v1.2.0-rc.1', 'v1.2.0-rc.2'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range(true)).resolves.toEqual({ from: 'v1.2.0-rc.2', to: 'HEAD' });
+
+    tags = ['v1.0.0', 'v1.1.0-rc.0', 'v1.1.0', 'v1.2.0-rc.0', 'v1.2.0-rc.1'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range(true)).resolves.toEqual({ from: 'v1.2.0-rc.1', to: 'HEAD' });
+  });
+
+  test('a valid range object even though tags are given in a not sorted semver order', async () => {
+    expect.assertions(1);
+
+    let tags = ['v1.1.0', 'v1.1.0-rc.0', 'v1.0.0', 'v1.1.0-rc.1'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range(true)).resolves.toEqual({ from: 'v1.1.0', to: 'HEAD' });
+  });
+
+  test('a valid range object filtering out any tags given a not semver compatible name', async () => {
+    expect.assertions(1);
+
+    let tags = ['v1.1.0', 'exp-0', 'v1.2.0-alpha.0', 'v1.1.0-rc.0', 'v1.0.0', 'v1.1.0-rc.1', 'stable'];
+    execFile.mockReturnValue(Promise.resolve({ stdout: tags.join('\n') }));
+
+    await expect(range(true)).resolves.toEqual({ from: 'v1.2.0-alpha.0', to: 'HEAD' });
   });
 });
