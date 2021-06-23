528bd51384a42eb2d631004f8809a4082a254442
Jake Ob <iakopap@gmail.com>
2021-06-23 16:08:40 +0300

    Rename the git log operation to simple log

--- test/log.test.js
+++ test/log.test.js
@@ -6,26 +6,26 @@ const { execFile } = require('child_process');
 
 promisify.mockImplementation((fn) => fn);
 
-const gitlog = require('../lib/gitlog.js');
+const log = require('../lib/log.js');
 
 afterEach(() => {
   execFile.mockReset();
 });
 
-describe('Gitlog should reject with Error', () => {
+describe('Log should reject with Error', () => {
   test('when `from` arg is anything but a non empty string', async () => {
     expect.assertions(11);
 
-    await expect(gitlog({ from: 123 })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: NaN })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: Infinity })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: true })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: false })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: [] })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: {} })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: (() => {}) })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: Symbol('sym') })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: '' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: 123 })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: NaN })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: Infinity })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: true })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: false })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: [] })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: {} })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: (() => {}) })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: Symbol('sym') })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: '' })).rejects.toBeInstanceOf(Error);
 
     expect(execFile).toHaveBeenCalledTimes(0);
   });
@@ -33,12 +33,12 @@ describe('Gitlog should reject with Error', () => {
   test('when `from` is a not valid git commit hash (sha1) or semver tag name', async () => {
     expect.assertions(7);
 
-    await expect(gitlog({ from: 'c26GGG' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: 'c262349^' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: '^c262349' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: '01.33.a4' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: '..' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ from: '...' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: 'c26GGG' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: 'c262349^' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: '^c262349' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: '01.33.a4' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: '..' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ from: '...' })).rejects.toBeInstanceOf(Error);
 
     expect(execFile).toHaveBeenCalledTimes(0);
   });
@@ -46,16 +46,16 @@ describe('Gitlog should reject with Error', () => {
   test('when `to` arg is anything but a non empty string', async () => {
     expect.assertions(11);
 
-    await expect(gitlog({ to: 123 })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: NaN })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: Infinity })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: true })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: false })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: [] })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: {} })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: (() => {}) })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: Symbol('sym') })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: '' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: 123 })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: NaN })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: Infinity })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: true })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: false })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: [] })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: {} })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: (() => {}) })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: Symbol('sym') })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: '' })).rejects.toBeInstanceOf(Error);
 
     expect(execFile).toHaveBeenCalledTimes(0);
   });
@@ -63,12 +63,12 @@ describe('Gitlog should reject with Error', () => {
   test('when `to` is a not valid git commit hash (sha1) or semver tag name', async () => {
     expect.assertions(7);
 
-    await expect(gitlog({ to: 'c26GGG' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: 'c262349^' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: '^c262349' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: '01.33.a4' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: '..' })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ to: '...' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: 'c26GGG' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: 'c262349^' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: '^c262349' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: '01.33.a4' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: '..' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ to: '...' })).rejects.toBeInstanceOf(Error);
 
     expect(execFile).toHaveBeenCalledTimes(0);
   });
@@ -76,16 +76,16 @@ describe('Gitlog should reject with Error', () => {
   test('when `format` arg is anything but a non empty string', async () => {
     expect.assertions(11);
 
-    await expect(gitlog({ format: 123 })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: NaN })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: Infinity })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: true })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: false })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: [] })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: {} })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: (() => {}) })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: Symbol('sym') })).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({ format: '' })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: 123 })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: NaN })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: Infinity })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: true })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: false })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: [] })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: {} })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: (() => {}) })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: Symbol('sym') })).rejects.toBeInstanceOf(Error);
+    await expect(log({ format: '' })).rejects.toBeInstanceOf(Error);
 
     expect(execFile).toHaveBeenCalledTimes(0);
   });
@@ -95,8 +95,8 @@ describe('Gitlog should reject with Error', () => {
 
     execFile.mockReturnValue(Promise.resolve({ stderr: new Error('A stderr occurred') }));
 
-    await expect(gitlog({})).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({})).rejects.toHaveProperty('message', 'Error: A stderr occurred');
+    await expect(log({})).rejects.toBeInstanceOf(Error);
+    await expect(log({})).rejects.toHaveProperty('message', 'Error: A stderr occurred');
 
     expect(execFile).toHaveBeenCalledTimes(2);
   });
@@ -106,8 +106,8 @@ describe('Gitlog should reject with Error', () => {
 
     execFile.mockReturnValue(Promise.resolve({ stderr: 'A stderr occurred' }));
 
-    await expect(gitlog({})).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({})).rejects.toHaveProperty('message', 'A stderr occurred');
+    await expect(log({})).rejects.toBeInstanceOf(Error);
+    await expect(log({})).rejects.toHaveProperty('message', 'A stderr occurred');
 
     expect(execFile).toHaveBeenCalledTimes(2);
   });
@@ -117,14 +117,14 @@ describe('Gitlog should reject with Error', () => {
 
     execFile.mockReturnValue(Promise.reject(new Error('A fatal error occurred')));
 
-    await expect(gitlog({})).rejects.toBeInstanceOf(Error);
-    await expect(gitlog({})).rejects.toHaveProperty('message', 'A fatal error occurred');
+    await expect(log({})).rejects.toBeInstanceOf(Error);
+    await expect(log({})).rejects.toHaveProperty('message', 'A fatal error occurred');
 
     expect(execFile).toHaveBeenCalledTimes(2);
   });
 });
 
-describe('Gitlog should spawn once a git log process in `--oneline` mode', () => {
+describe('Log should spawn once a git log process in `--oneline` mode', () => {
   beforeEach(() => {
     execFile.mockReturnValue(Promise.resolve({ stdout: '' }));
   });
@@ -132,7 +132,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with only `--format` set to default when no options argument is given', async () => {
     expect.assertions(2);
 
-    await gitlog();
+    await log();
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -141,7 +141,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with only `--format` set to default when an empty options argument is given', async () => {
     expect.assertions(2);
 
-    await gitlog({});
+    await log({});
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -150,7 +150,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with only `--format` set to default when options argument is given with nullish properties', async () => {
     expect.assertions(2);
 
-    await gitlog({ from: null, to: null, format: null });
+    await log({ from: null, to: null, format: null });
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -159,7 +159,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with `--format` set to default when options is given with nullish `format`', async () => {
     expect.assertions(2);
 
-    await gitlog({ format: null });
+    await log({ format: null });
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -168,7 +168,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with `--format` set equal to the given `format` property in the options', async () => {
     expect.assertions(2);
 
-    await gitlog({ format: '%s %an' });
+    await log({ format: '%s %an' });
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%s %an']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -177,7 +177,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with a range notation `from..to` set to the given `from` and `to` properties in the options', async () => {
     expect.assertions(2);
 
-    await gitlog({ from: '871647f', to: '84e2fa8' });
+    await log({ from: '871647f', to: '84e2fa8' });
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s', '871647f..84e2fa8']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -186,7 +186,7 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with a range notation `from..` set to the given `from` property in the options', async () => {
     expect.assertions(2);
 
-    await gitlog({ from: '871647f' });
+    await log({ from: '871647f' });
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s', '871647f..']);
     expect(execFile).toHaveBeenCalledTimes(1);
@@ -195,24 +195,24 @@ describe('Gitlog should spawn once a git log process in `--oneline` mode', () =>
   test('with a range notation `to` set to the given `to` property in the options', async () => {
     expect.assertions(2);
 
-    await gitlog({ to: '84e2fa8' });
+    await log({ to: '84e2fa8' });
 
     expect(execFile).toHaveBeenCalledWith('git', ['log', '--oneline', '--format=%h %s', '84e2fa8']);
     expect(execFile).toHaveBeenCalledTimes(1);
   });
 });
 
-describe('Gitlog should resolve to', () => {
+describe('Log should resolve to', () => {
   test('an array of strings when git log process returns a successful stdout', async () => {
     expect.assertions(2);
 
     execFile.mockReturnValue(Promise.resolve({ stdout: `line1\n` }));
 
-    await expect(gitlog()).resolves.toEqual(['line1']);
+    await expect(log()).resolves.toEqual(['line1']);
 
     execFile.mockReturnValue(Promise.resolve({ stdout: `line1\nline2\nline3\n` }));
 
-    await expect(gitlog()).resolves.toEqual(['line1', 'line2', 'line3']);
+    await expect(log()).resolves.toEqual(['line1', 'line2', 'line3']);
   });
 
   test('an empty array when git log process returns a successful but empty stdout', async () => {
@@ -220,10 +220,10 @@ describe('Gitlog should resolve to', () => {
 
     execFile.mockReturnValue(Promise.resolve({ stdout: `\n` }));
 
-    await expect(gitlog()).resolves.toEqual([]);
+    await expect(log()).resolves.toEqual([]);
 
     execFile.mockReturnValue(Promise.resolve({ stdout: `` }));
 
-    await expect(gitlog()).resolves.toEqual([]);
+    await expect(log()).resolves.toEqual([]);
   });
 });
