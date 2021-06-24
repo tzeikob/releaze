6aacd817b2b87517175c4d60877c081dab1b3e85
Jake Ob <iakopap@gmail.com>
2021-06-24 19:54:51 +0300

    Add changelog op to build the CHANGELOG.md (refs #12)
    The changelog operation should collect each log into markdown bullet
    list items and the version should come first as single (`#`) header.
    In case there is an existing `CHANGELOG.md` file, the new logs should
    be added to the top of the file.
    
    Refs #12

---
+++ test/changelog.test.js
@@ -0,0 +1,191 @@
+jest.mock('fs', () => ({
+  promises: {
+    readFile: jest.fn().mockResolvedValue(),
+    writeFile: jest.fn().mockResolvedValue(),
+  }
+}));
+
+const fs = require('fs');
+const path = require('path');
+const changelog = require('../lib/changelog');
+
+const { readFile, writeFile } = fs.promises;
+
+const pathToChangelogMD = path.join(process.cwd(), 'CHANGELOG.md');
+
+afterEach(() => {
+  readFile.mockReset();
+  writeFile.mockReset();
+});
+
+describe('Changelog should reject with an error', () => {
+  test('when called with missing `version` argument', async () => {
+    expect.assertions(3);
+
+    const reason = 'Invalid or missing version argument';
+
+    await expect(changelog()).rejects.toThrow(reason);
+
+    expect(readFile).toHaveBeenCalledTimes(0);
+    expect(writeFile).toHaveBeenCalledTimes(0);
+  });
+
+  test('when called with invalid non-string `version` argument', async () => {
+    expect.assertions(10);
+
+    const reason = 'Invalid or missing version argument';
+
+    await expect(changelog(123)).rejects.toThrow(reason);
+    await expect(changelog(true)).rejects.toThrow(reason);
+    await expect(changelog(false)).rejects.toThrow(reason);
+    await expect(changelog('')).rejects.toThrow(reason);
+    await expect(changelog({})).rejects.toThrow(reason);
+    await expect(changelog(Symbol('s'))).rejects.toThrow(reason);
+    await expect(changelog(() => {})).rejects.toThrow(reason);
+    await expect(changelog(null)).rejects.toThrow(reason);
+
+    expect(readFile).toHaveBeenCalledTimes(0);
+    expect(writeFile).toHaveBeenCalledTimes(0);
+  });
+
+  test('when called with no `logs` argument', async () => {
+    expect.assertions(3);
+
+    const reason = 'Invalid or missing logs argument';
+
+    await expect(changelog('v1.0.0')).rejects.toThrow(reason);
+
+    expect(readFile).toHaveBeenCalledTimes(0);
+    expect(writeFile).toHaveBeenCalledTimes(0);
+  });
+
+  test('when called with invalid `logs` argument', async () => {
+    expect.assertions(11);
+
+    const reason = 'Invalid or missing logs argument';
+
+    await expect(changelog('v1.0.0', 123)).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', true)).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', false)).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', '')).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', 'logs')).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', {})).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', Symbol('s'))).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', () => {})).rejects.toThrow(reason);
+    await expect(changelog('v1.0.0', null)).rejects.toThrow(reason);
+
+    expect(readFile).toHaveBeenCalledTimes(0);
+    expect(writeFile).toHaveBeenCalledTimes(0);
+  });
+
+  test('when called with `logs` argument given as an empty array', async () => {
+    expect.assertions(3);
+
+    const reason = 'Invalid or missing logs argument';
+
+    await expect(changelog('v1.0.0', [])).rejects.toThrow(reason);
+
+    expect(readFile).toHaveBeenCalledTimes(0);
+    expect(writeFile).toHaveBeenCalledTimes(0);
+  });
+
+  test('when reading from `CHANGELOG.md` failed for a fatal reason', async () => {
+    expect.assertions(1);
+
+    const error = new Error('Unknown fatal error occurred reading from CHANGELOG.md');
+
+    readFile.mockReturnValue(Promise.reject(error));
+
+    await expect(changelog('v1.0.0', ['log1', 'log2'])).rejects.toThrow(error);
+  });
+});
+
+describe('Changelog called with valid `version` and `logs` should', () => {
+  test('create a new `CHANGELOG.md` file if not yet exists', async () => {
+    expect.assertions(5);
+
+    const error = new Error("ENOENT: no such file or directory, open 'CHANGELOG.md'");
+    error.code = 'ENOENT';
+
+    readFile.mockReturnValue(Promise.reject(error));
+    writeFile.mockReturnValue(Promise.resolve());
+
+    await expect(changelog('v1.0.0', ['log1', 'log2'])).resolves.toBeUndefined();
+
+    expect(readFile).toHaveBeenCalledTimes(1);
+    expect(readFile).toHaveBeenCalledWith(pathToChangelogMD, 'utf8');
+
+    expect(writeFile).toHaveBeenCalledTimes(1);
+    expect(writeFile).toHaveBeenCalledWith(pathToChangelogMD, expect.any(String));
+  });
+
+  test('write once to the existing `CHANGELOG.md` file if such already exists', async () => {
+    expect.assertions(5);
+    
+    readFile.mockReturnValue(Promise.resolve('log1'));
+    writeFile.mockReturnValue(Promise.resolve());
+
+    await expect(changelog('v1.0.0', ['log2', 'log3'])).resolves.toBeUndefined();
+
+    expect(readFile).toHaveBeenCalledTimes(1);
+    expect(readFile).toHaveBeenCalledWith(pathToChangelogMD, 'utf8');
+
+    expect(writeFile).toHaveBeenCalledTimes(1);
+    expect(writeFile).toHaveBeenCalledWith(pathToChangelogMD, expect.any(String));
+  });
+
+  test('write the logs to a new `CHANGELOG.md` in the correct format', async () => {
+    expect.assertions(3);
+
+    const error = new Error("ENOENT: no such file or directory, open 'CHANGELOG.md'");
+    error.code = 'ENOENT';
+
+    readFile.mockReturnValue(Promise.reject(error));
+    writeFile.mockReturnValue(Promise.resolve());
+
+    const logs = [
+      '54ff0cd Restrict bump release types to lowercase only',
+      'd41ab22 Accept alias HEAD as input to the log op'
+    ];
+
+    await expect(changelog('v2.0.0', logs)).resolves.toBeUndefined();
+
+    const newContent = [
+      '#v2.0.0',
+      '* 54ff0cd Restrict bump release types to lowercase only',
+      '* d41ab22 Accept alias HEAD as input to the log op'
+    ].join('\n');
+
+    expect(writeFile).toHaveBeenCalledWith(pathToChangelogMD, `${newContent}`);
+    expect(writeFile).toHaveBeenCalledTimes(1);
+  });
+
+  test('append the logs on top to an existing `CHANGELOG.md` in the correct format', async () => {
+    expect.assertions(3);
+
+    content = [
+      '#v1.5.2',
+      '* 81ad3fb Refactor log tests to assert every await call',
+      '* 3b5b25f Refactor log tests to assert with toThrow instead'
+    ].join('\n');
+
+    readFile.mockReturnValue(Promise.resolve(content));
+    writeFile.mockReturnValue(Promise.resolve());
+
+    const logs = [
+      '54ff0cd Restrict bump release types to lowercase only',
+      'd41ab22 Accept alias HEAD as input to the log op'
+    ];
+
+    await expect(changelog('v2.0.0', logs)).resolves.toBeUndefined();
+
+    const newContent = [
+      '#v2.0.0',
+      '* 54ff0cd Restrict bump release types to lowercase only',
+      '* d41ab22 Accept alias HEAD as input to the log op'
+    ].join('\n');
+
+    expect(writeFile).toHaveBeenCalledWith(pathToChangelogMD, `${newContent}\n${content}`);
+    expect(writeFile).toHaveBeenCalledTimes(1);
+  });
+});
