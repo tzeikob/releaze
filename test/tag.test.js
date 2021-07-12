affd960d19944c3d9915542e579c87b529ddc397
Jake Ob <iakopap@gmail.com>
2021-07-12 15:30:45 +0300

    Simplify the tag op specs
    The tag operation should be a bit loose in terms of,
    
     * message could be any value as long as it converts to String
     * package lock files should staged even though are missing
     * CHANGELOG.md file could be missing
    
    Reduce deps by removing fs I/O calls as much as possible.

--- test/tag.test.js
+++ test/tag.test.js
@@ -1,27 +1,17 @@
-const fs = require('fs');
 const semver = require('semver');
 const exec = require('../lib/util/exec');
-const ExecError = require('../lib/errors/exec-error');
 const tag = require('../lib/tag.js');
-
-jest.mock('fs', () => ({
-  promises: {
-    access: jest.fn().mockResolvedValue()
-  }
-}));
 
 jest.mock('../lib/util/exec', () => jest.fn().mockResolvedValue());
 
-const { access } = fs.promises;
 const { any } = expect;
 
 afterEach(() => {
-  access.mockReset();
   exec.mockReset();
 });
 
 describe('Tag should resolve to undefined', () => {
-  test('have add, commit and tag git commands executed in a strict order', async () => {
+  test('have git add, commit and tag commands executed in that given order', async () => {
     expect.assertions(8);
 
     await expect(tag('1.0.0')).resolves.toBeUndefined();
@@ -37,31 +27,52 @@ describe('Tag should resolve to undefined', () => {
     expect(exec).nthCalledWith(6, 'git', ['tag', '-a', any(String), '-m', any(String)]);
   });
 
-  test('have missing CHANGELOG.md, package-lock.json and npm-shrinkwrap.json skipped from staging', async () => {
-    expect.assertions(12);
+  test('even if the CHANGELOG file is missing or failed to be staged', async () => {
+    expect.assertions(8);
 
-    access.mockRejectedValueOnce('CHANGELOG.md not found');
-    access.mockRejectedValueOnce('package-lock.json not found');
-    access.mockRejectedValueOnce('npm-shrinkwrap.json not found');
+    exec
+      .mockResolvedValueOnce('package.json staged successfully')
+      .mockRejectedValueOnce(new Error('failed to stage missing CHANGELOG.md'))
+      .mockResolvedValueOnce('package-lock.json staged successfully')
+      .mockResolvedValueOnce('npm-shrinkwrap.json staged successfully')
+      .mockResolvedValueOnce('commit changes successfully')
+      .mockResolvedValueOnce('tag created successfully');
 
     await expect(tag('1.0.0')).resolves.toBeUndefined();
 
-    expect(access).toBeCalledTimes(3);
-    expect(exec).toBeCalledTimes(3);
+    expect(exec).toBeCalledTimes(6);
 
     expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
+    expect(exec).nthCalledWith(2, 'git', ['add', 'CHANGELOG.md']);
+    expect(exec).nthCalledWith(3, 'git', ['add', 'package-lock.json']);
+    expect(exec).nthCalledWith(4, 'git', ['add', 'npm-shrinkwrap.json']);
 
-    expect(access).nthCalledWith(1, 'CHANGELOG.md');
-    expect(exec).not.toBeCalledWith('git', ['add', 'CHANGELOG.md']);
+    expect(exec).nthCalledWith(5, 'git', ['commit', '-m', any(String)]);
+    expect(exec).nthCalledWith(6, 'git', ['tag', '-a', any(String), '-m', any(String)]);
+  });
 
-    expect(access).nthCalledWith(2, 'package-lock.json');
-    expect(exec).not.toBeCalledWith('git', ['add', 'package-lock.json']);
+  test('even if package lock files are missing or failed to be staged', async () => {
+    expect.assertions(8);
 
-    expect(access).nthCalledWith(3, 'npm-shrinkwrap.json');
-    expect(exec).not.toBeCalledWith('git', ['add', 'npm-shrinkwrap.json']);
+    exec
+      .mockResolvedValueOnce('package.json staged successfully')
+      .mockResolvedValueOnce('CHANGELOG.md staged successfully')
+      .mockRejectedValueOnce(new Error('failed to stage missing package-lock.json'))
+      .mockRejectedValueOnce(new Error('failed to stage missing npm-shrinkwrap.json'))
+      .mockResolvedValueOnce('commit changes successfully')
+      .mockResolvedValueOnce('tag created successfully');
 
-    expect(exec).nthCalledWith(2, 'git', ['commit', '-m', any(String)]);
-    expect(exec).nthCalledWith(3, 'git', ['tag', '-a', any(String), '-m', any(String)]);
+    await expect(tag('1.0.0')).resolves.toBeUndefined();
+
+    expect(exec).toBeCalledTimes(6);
+
+    expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
+    expect(exec).nthCalledWith(2, 'git', ['add', 'CHANGELOG.md']);
+    expect(exec).nthCalledWith(3, 'git', ['add', 'package-lock.json']);
+    expect(exec).nthCalledWith(4, 'git', ['add', 'npm-shrinkwrap.json']);
+
+    expect(exec).nthCalledWith(5, 'git', ['commit', '-m', any(String)]);
+    expect(exec).nthCalledWith(6, 'git', ['tag', '-a', any(String), '-m', any(String)]);
   });
 });
 
@@ -177,198 +188,91 @@ describe('Tag should try to create an annotation tag', () => {
 
 describe('Tag called with invalid input should reject early with error', () => {
   test('when version argument is not given', async () => {
-    expect.assertions(3);
+    expect.assertions(2);
 
     const reason = 'Invalid or missing semver version argument';
 
     await expect(tag()).rejects.toThrow(reason);
 
-    expect(access).toBeCalledTimes(0);
     expect(exec).toBeCalledTimes(0);
   });
 
   test('when invalid non semver version argument is given', async () => {
-    expect.assertions(14);
+    expect.assertions(5);
 
     const reason = 'Invalid or missing semver version argument';
 
     await expect(tag('123')).rejects.toThrow(reason);
+    await expect(tag('1.3')).rejects.toThrow(reason);
     await expect(tag('1.alpha.3')).rejects.toThrow(reason);
-    await expect(tag('head')).rejects.toThrow(reason);
-    await expect(tag('HEAD')).rejects.toThrow(reason);
+    await expect(tag('alpha')).rejects.toThrow(reason);
 
-    await expect(tag(123)).rejects.toThrow(reason);
-    await expect(tag(NaN)).rejects.toThrow(reason);
-    await expect(tag(Infinity)).rejects.toThrow(reason);
-    await expect(tag(true)).rejects.toThrow(reason);
-
-    await expect(tag([])).rejects.toThrow(reason);
-    await expect(tag({})).rejects.toThrow(reason);
-    await expect(tag(Symbol('s'))).rejects.toThrow(reason);
-    await expect(tag(() => {})).rejects.toThrow(reason);
-
-    expect(access).toBeCalledTimes(0);
-    expect(exec).toBeCalledTimes(0);
-  });
-
-  test('when invalid non string template message argument is given', async () => {
-    expect.assertions(11);
-
-    const reason = 'Invalid non string message argument';
-
-    await expect(tag('1.0.0', '')).rejects.toThrow(reason);
-    await expect(tag('1.0.0', 123)).rejects.toThrow(reason);
-    await expect(tag('1.0.0', NaN)).rejects.toThrow(reason);
-    await expect(tag('1.0.0', Infinity)).rejects.toThrow(reason);
-    await expect(tag('1.0.0', true)).rejects.toThrow(reason);
-
-    await expect(tag('1.0.0', [])).rejects.toThrow(reason);
-    await expect(tag('1.0.0', {})).rejects.toThrow(reason);
-    await expect(tag('1.0.0', Symbol('s'))).rejects.toThrow(reason);
-    await expect(tag('1.0.0', () => {})).rejects.toThrow(reason);
-
-    expect(access).toBeCalledTimes(0);
     expect(exec).toBeCalledTimes(0);
   });
 });
 
 describe('Tag should reject early with error', () => {
   test('when `git add package.json` throws a fatal exec error', async () => {
-    expect.assertions(4);
+    expect.assertions(3);
 
     const reason = 'An error occurred executing `git add package.json`';
-    exec.mockRejectedValueOnce(new ExecError(reason));
+
+    exec.mockRejectedValueOnce(new Error(reason));
 
     await expect(tag('1.0.0')).rejects.toThrow(reason);
 
-    expect(access).toBeCalledTimes(0);
     expect(exec).toBeCalledTimes(1);
 
     expect(exec).toBeCalledWith('git', ['add', 'package.json']);
   });
 
-  test('when `git add CHANGELOG.md` throws a fatal exec error', async () => {
-    expect.assertions(6);
-
-    exec.mockResolvedValueOnce('package.json staged successfully');
-
-    access.mockResolvedValueOnce('CHANGELOG.md exists');
-    const reason = 'An error occurred executing `git add CHANGELOG.md`';
-    exec.mockRejectedValueOnce(new ExecError(reason));
-
-    await expect(tag('1.0.0')).rejects.toThrow(reason);
-
-    expect(access).toBeCalledTimes(1);
-    expect(exec).toBeCalledTimes(2);
-
-    expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
-
-    expect(access).toBeCalledWith('CHANGELOG.md');
-    expect(exec).nthCalledWith(2, 'git', ['add', 'CHANGELOG.md']);
-  });
-
-  test('when `git add package-lock.json` throws a fatal exec error', async () => {
+  test('when `git commit` throws a fatal exec error', async () => {
     expect.assertions(7);
 
-    exec.mockResolvedValueOnce('package.json staged successfully');
-
-    access.mockRejectedValueOnce(new Error('CHANGELOG.md file not found'));
-
-    access.mockResolvedValueOnce('package-lock.json exists');
-    const reason = 'An error occurred executing `git add package-lock.js`';
-    exec.mockRejectedValueOnce(new ExecError(reason));
-
-    await expect(tag('1.0.0')).rejects.toThrow(reason);
-
-    expect(access).toBeCalledTimes(2);
-    expect(exec).toBeCalledTimes(2);
-
-    expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
-
-    expect(access).nthCalledWith(1, 'CHANGELOG.md');
-
-    expect(access).nthCalledWith(2, 'package-lock.json');
-    expect(exec).nthCalledWith(2, 'git', ['add', 'package-lock.json']);
-  });
-
-  test('when `git add npm-shrinkwrap.json` throws a fatal exec error', async () => {
-    expect.assertions(8);
-
-    exec.mockResolvedValueOnce('package.json staged successfully');
-
-    access.mockRejectedValueOnce(new Error('CHANGELOG.md file not found'));
-    access.mockRejectedValueOnce(new Error('package-lock.json file not found'));
-
-    access.mockResolvedValueOnce('npm-shrinkwrap.json exists');
-    const reason = 'An error occurred executing `git add npm-shrinkwrap.json`';
-    exec.mockRejectedValueOnce(new ExecError(reason));
-
-    await expect(tag('1.0.0')).rejects.toThrow(reason);
-
-    expect(access).toBeCalledTimes(3);
-    expect(exec).toBeCalledTimes(2);
-
-    expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
-
-    expect(access).nthCalledWith(1, 'CHANGELOG.md');
-    expect(access).nthCalledWith(2, 'package-lock.json');
-
-    expect(access).nthCalledWith(3, 'npm-shrinkwrap.json');
-    expect(exec).nthCalledWith(2, 'git', ['add', 'npm-shrinkwrap.json']);
-  });
-
-  test('when `git commit` throws a fatal exec error', async () => {
-    expect.assertions(8);
-  
-    exec.mockResolvedValueOnce('package.json staged successfully');
-    
-    access.mockRejectedValueOnce('CHANGELOG.md not found');
-    access.mockRejectedValueOnce('package-lock.json not found');
-    access.mockRejectedValueOnce('npm-shrinkwrap.json not found');
-  
     const reason = 'An error occurred executing git commit';
-    exec.mockRejectedValueOnce(new ExecError(reason));
-  
+
+    exec
+      .mockResolvedValueOnce('package.json staged successfully')
+      .mockResolvedValueOnce('CHANGELOG.md staged successfully')
+      .mockResolvedValueOnce('package-lock.json staged successfully')
+      .mockResolvedValueOnce('npm-shrinkwrap.json staged successfully')
+      .mockRejectedValueOnce(new Error(reason));
+
     await expect(tag('1.0.0')).rejects.toThrow(reason);
 
-    expect(access).toBeCalledTimes(3);
-    expect(exec).toBeCalledTimes(2);
+    expect(exec).toBeCalledTimes(5);
   
     expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
+    expect(exec).nthCalledWith(2, 'git', ['add', 'CHANGELOG.md']);
+    expect(exec).nthCalledWith(3, 'git', ['add', 'package-lock.json']);
+    expect(exec).nthCalledWith(4, 'git', ['add', 'npm-shrinkwrap.json']);
 
-    expect(access).nthCalledWith(1, 'CHANGELOG.md');
-    expect(access).nthCalledWith(2, 'package-lock.json');
-    expect(access).nthCalledWith(3, 'npm-shrinkwrap.json');
-
-    expect(exec).nthCalledWith(2, 'git', ['commit', '-m', any(String)]);
+    expect(exec).nthCalledWith(5, 'git', ['commit', '-m', any(String)]);
   });
 
   test('when `git tag` throws a fatal exec error', async () => {
-    expect.assertions(9);
-
-    exec.mockResolvedValueOnce('package.json staged successfully');
-
-    access.mockRejectedValueOnce('CHANGELOG.md not found');
-    access.mockRejectedValueOnce('package-lock.json not found');
-    access.mockRejectedValueOnce('npm-shrinkwrap.json not found');
-
-    exec.mockResolvedValueOnce('commit changes successfully');
+    expect.assertions(8);
 
     const reason = 'An error occurred executing git tag';
-    exec.mockRejectedValueOnce(new ExecError(reason));
+
+    exec
+      .mockResolvedValueOnce('package.json staged successfully')
+      .mockResolvedValueOnce('CHANGELOG.md staged successfully')
+      .mockResolvedValueOnce('package-lock.json staged successfully')
+      .mockResolvedValueOnce('npm-shrinkwrap.json staged successfully')
+      .mockResolvedValueOnce('commit changes successfully')
+      .mockRejectedValueOnce(new Error(reason));
 
     await expect(tag('1.0.0')).rejects.toThrow(reason);
 
-    expect(access).toBeCalledTimes(3);
-    expect(exec).toBeCalledTimes(3);
+    expect(exec).toBeCalledTimes(6);
 
     expect(exec).nthCalledWith(1, 'git', ['add', 'package.json']);
+    expect(exec).nthCalledWith(2, 'git', ['add', 'CHANGELOG.md']);
+    expect(exec).nthCalledWith(3, 'git', ['add', 'package-lock.json']);
+    expect(exec).nthCalledWith(4, 'git', ['add', 'npm-shrinkwrap.json']);
 
-    expect(access).nthCalledWith(1, 'CHANGELOG.md');
-    expect(access).nthCalledWith(2, 'package-lock.json');
-    expect(access).nthCalledWith(3, 'npm-shrinkwrap.json');
-
-    expect(exec).nthCalledWith(2, 'git', ['commit', '-m', any(String)]);
-    expect(exec).nthCalledWith(3, 'git', ['tag', '-a', any(String), '-m', any(String)]);
+    expect(exec).nthCalledWith(5, 'git', ['commit', '-m', any(String)]);
+    expect(exec).nthCalledWith(6, 'git', ['tag', '-a', any(String), '-m', any(String)]);
   });
 });
